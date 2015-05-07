a9a8983b2676960ddb31808c7b53feedb92ad40d
Willy Maikowski <willy.maikowski@gmail.com>
2015-05-07 15:14:47 -0300

    Clear

--- etl.py
+++ etl.py
@@ -1,127 +1,114 @@
 # -*- coding: cp1252 -*-
 import xml.etree.ElementTree as ET
-from xml.dom import minidom
+import lxml.etree as etree
 import time
 
-base_uri = 'http://datos.uchile.cl/'
+BASE_URI = 'http://datos.uchile.cl/'
 
-# Base element for authorities
-output_person_root = ET.Element('rdf:RDF', {'xmlns:owl': base_uri + 'ontologia/',
-                                            'xmlns:foaf': 'http://xmlns.com/foaf/0.1/',
-                                            'xmlns:bio': 'http://vocab.org/bio/0.1/',
-                                            'xmlns:rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'})
+dic = {
+    'xmlns:owl': BASE_URI + 'ontologia/',
+    'xmlns:foaf': 'http://xmlns.com/foaf/0.1/',
+    'xmlns:bio': 'http://vocab.org/bio/0.1/',
+    'xmlns:rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+}
 
-# Base element for years
-output_year_root = ET.Element('rdf:RDF', {'xmlns:owl': base_uri + 'ontologia/',
-                                         'xmlns:foaf': 'http://xmlns.com/foaf/0.1/',
-                                         'xmlns:bio': 'http://vocab.org/bio/0.1/',
-                                         'xmlns:rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'})
+output = {
+    'person_root' : ET.Element( 'rdf:RDF', dic ), #Base element for authorities
+    'year_root' : ET.Element( 'rdf:RDF', dic ) #Years
+}
 
-birthYearDict = {}
-deathYearDict = {}
 
+birthYearDict = {}; deathYearDict = {}
 i = 0
+for event, elem in ET.iterparse( 'input/autoridades-big.xml', events=( 'start', 'end', 'start-ns', 'end-ns' ) ):
+    if event != 'start' or elem.tag != 'authority': continue
+    authority = elem
+    authIDElement = authority.find('authorityID')
 
-for event, elem in ET.iterparse('input/autoridades-big.xml', events=('start', 'end', 'start-ns', 'end-ns')):
-    #print event, elem
-    if event == 'start' and elem.tag == 'authority':
-        authority = elem
-        authIDElement = authority.find('authorityID')
+    if authIDElement == None or authIDElement.text == None: continue
+    authID = authIDElement.text
 
-        if authIDElement != None:
-            authID = authIDElement.text
-            if authID == None:
-                continue
-        else:
-            continue
+    personElement = ET.SubElement( output['person_root'], 'owl:NamedIndividual', {'rdf:about': BASE_URI + 'autoridad/' + authID} )
 
-        personElement = ET.SubElement(output_person_root, 'owl:NamedIndividual', {'rdf:about': base_uri + 'autoridad/' + authID})
+    nameElement = authority.find( ".//*[@tag='100']" )
+    if nameElement == None or nameElement.text == None: continue
+    
+    marcEntryTextArray = nameElement.text.split('|')
+    if len(marcEntryTextArray) == 1: continue
 
-        nameElement = authority.find(".//*[@tag='100']")
-        if nameElement != None:
-            if nameElement.text != None:
-                marcEntryTextArray = nameElement.text.split('|')
+    for entry in marcEntryTextArray:
+        if entry == '': continue
 
-                if len(marcEntryTextArray) == 1: continue
+        # caso nombre
+        if entry[0] == 'a':
+            # arreglo apellido - nombre
+            nameArray = entry[1:].split(',')
 
-                for entry in marcEntryTextArray:
-                    if entry == '': continue
+            # arreglo nombre - apellido
+            nameArray = nameArray[::-1]
 
-                    # caso nombre
-                    if entry[0] == 'a':
-                        # arreglo apellido - nombre
-                        nameArray = entry[1:].split(',')
+            # quitar espacios sobrantes por elemento
+            nameArray = map(lambda e : e.strip() ,nameArray)
 
-                        # arreglo nombre - apellido
-                        nameArray = nameArray[::-1]
+            # quitar elementos vacios del arreglo
+            nameArray = filter(lambda e : e != '', nameArray)
 
-                        # quitar espacios sobrantes por elemento
-                        nameArray = map(lambda e : e.strip() ,nameArray)
+            # crear elemento persona
+            nameElement = ET.SubElement(personElement, 'foaf:name')
+            nameElement.text = ' '.join(nameArray)
 
-                        # quitar elementos vacios del arreglo
-                        nameArray = filter(lambda e : e != '', nameArray)
+        # caso fechas
+        if entry[0] == 'd':
 
-                        # crear elemento persona
-                        nameElement = ET.SubElement(personElement, 'foaf:name')
-                        nameElement.text = ' '.join(nameArray)
+            # arreglo de fechas nacimiento - muerte
+            yearTextArray = entry[1:].split('-')
 
-                    # caso fechas
-                    if entry[0] == 'd':
+            # ver si la fecha es parseable
+            # Sacar puntos, comas, espacios sobrantes
+            # TODO: anhadir casos n. 1234 es nacimiento
+            # TODO: anhadir casos m. 1234 es muerte
+            # TODO: ver caso de fechas con 3 numeros
+            for yearIndex in range(len(yearTextArray)):
+                try:
+                    time.strptime(yearTextArray[yearIndex].strip().strip(',').strip('.'), "%Y").tm_year
+                    # fecha valida
+                    yearTextArray[yearIndex] = yearTextArray[yearIndex].strip().strip(',').strip('.')
+                except ValueError:
+                    # fecha invalida
+                    yearTextArray[yearIndex] = ''
 
-                        # arreglo de fechas nacimiento - muerte
-                        yearTextArray = entry[1:].split('-')
+            if yearTextArray[0] != '' and yearTextArray[0] not in birthYearDict:
+                # crear elemento anho nacimiento en personas
+                birthEventUri = BASE_URI + 'nacimiento/' + yearTextArray[0]
+                ET.SubElement(personElement, 'bio:event', {'rdf:resource': birthEventUri})
 
-                        # ver si la fecha es parseable
-                        # Sacar puntos, comas, espacios sobrantes
-                        # TODO: anhadir casos n. 1234 es nacimiento
-                        # TODO: anhadir casos m. 1234 es muerte
-                        # TODO: ver caso de fechas con 3 numeros
-                        for yearIndex in range(len(yearTextArray)):
-                            try:
-                                time.strptime(yearTextArray[yearIndex].strip().strip(',').strip('.'), "%Y").tm_year
-                                # fecha valida
-                                yearTextArray[yearIndex] = yearTextArray[yearIndex].strip().strip(',').strip('.')
-                            except ValueError:
-                                # fecha invalida
-                                yearTextArray[yearIndex] = ''
+                # crear elemento anho nacimiento en fechas
+                yearElement = ET.SubElement(output['year_root'], 'owl:NamedIndividual', {'rdf:about': birthEventUri})
+                ET.SubElement(yearElement, 'rdf:type', {'rdf:resource': 'http://purl.org/vocab/bio/0.1/Birth'})
+                ET.SubElement(yearElement, 'bio:date').text = yearTextArray[0]
 
-                        if yearTextArray[0] != '' and yearTextArray[0] not in birthYearDict:
-                            # crear elemento anho nacimiento en personas
-                            birthEventUri = base_uri + 'nacimiento/' + yearTextArray[0]
-                            ET.SubElement(personElement, 'bio:event', {'rdf:resource': birthEventUri})
+                # agregar al diccionario
+                birthYearDict[yearTextArray[0]] = ''
 
-                            # crear elemento anho nacimiento en fechas
-                            yearElement = ET.SubElement(output_year_root, 'owl:NamedIndividual', {'rdf:about': birthEventUri})
-                            ET.SubElement(yearElement, 'rdf:type', {'rdf:resource': 'http://purl.org/vocab/bio/0.1/Birth'})
-                            ET.SubElement(yearElement, 'bio:date').text = yearTextArray[0]
+            if len(yearTextArray) > 1:
+                if yearTextArray[1] != '' and yearTextArray[1] not in deathYearDict:
+                    # crear elemento anho muerte en personas
+                    deathEventUri = BASE_URI + 'muerte/' + yearTextArray[1]
+                    ET.SubElement(personElement, 'bio:event', {'rdf:resource': deathEventUri})
 
-                            # agregar al diccionario
-                            birthYearDict[yearTextArray[0]] = ''
+                    # crear elemento anho muerte en fechas
+                    yearElement = ET.SubElement(output['year_root'], 'owl:NamedIndividual', {'rdf:about': deathEventUri})
+                    ET.SubElement(yearElement, 'rdf:type', {'rdf:resource': 'http://purl.org/vocab/bio/0.1/Death'})
+                    ET.SubElement(yearElement, 'bio:date').text = yearTextArray[1]
 
-                        if len(yearTextArray) > 1:
-                            if yearTextArray[1] != '' and yearTextArray[1] not in deathYearDict:
-                                # crear elemento anho muerte en personas
-                                deathEventUri = base_uri + 'muerte/' + yearTextArray[1]
-                                ET.SubElement(personElement, 'bio:event', {'rdf:resource': deathEventUri})
-
-                                # crear elemento anho muerte en fechas
-                                yearElement = ET.SubElement(output_year_root, 'owl:NamedIndividual', {'rdf:about': deathEventUri})
-                                ET.SubElement(yearElement, 'rdf:type', {'rdf:resource': 'http://purl.org/vocab/bio/0.1/Death'})
-                                ET.SubElement(yearElement, 'bio:date').text = yearTextArray[1]
-
-                                # agregar al diccionario
-                                deathYearDict[yearTextArray[1]] = ''
-            else:
-                continue
-        else:
-            continue
+                    # agregar al diccionario
+                    deathYearDict[yearTextArray[1]] = ''
+    elem.clear();
     i += 1
-    # final prematuro
-    if i == 10000:
-        break
+    #if i > 100: break
 
-output_person_tree = ET.ElementTree(output_person_root)
-output_person_tree.write('output/personas.rdf', 'utf-8')
-
-output_year_tree = ET.ElementTree(output_year_root)
-output_year_tree.write('output/fechas.rdf', 'utf-8')
+print i
+ET.ElementTree( output['person_root'] ).write( 'output/personas.rdf', 'utf-8' )
+ET.ElementTree( output['year_root'] ).write( 'output/fechas.rdf', 'utf-8' )
+with open( 'output/personas.rdf', 'r' ) as ini, open( 'output/personas_pretty.rdf', 'w' ) as out: out.write( etree.tostring( etree.XML( ini.read() ), pretty_print = True ) )
+with open( 'output/fechas.rdf', 'r' ) as ini, open( 'output/fechas_pretty.rdf', 'w' ) as out: out.write( etree.tostring( etree.XML( ini.read() ), pretty_print = True ) )
