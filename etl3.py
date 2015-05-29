2e4f05050da346388be0c2d30aa1c76e931fc3c5
Fernando <fgmoralesc@gmail.com>
2015-05-29 11:43:34 -0300

    Replace rdf:Decription with owl:NamedIndividual

--- etl3.py
+++ etl3.py
@@ -40,7 +40,7 @@ for asset in input_root.iterfind("asset"):
         print i
         ID = asset.find('id').text
         #crear instancia de obra
-        obraElement = ET.SubElement(output_obra_root, 'rdf:Description', {'rdf:about': base_uri + 'recurso/obra/' + ID})
+        obraElement = ET.SubElement(output_obra_root, 'owl:NamedIndividual', {'rdf:about': base_uri + 'recurso/obra/' + ID})
         #agregar los subjects, puede haber mas de 1
         for subject in asset.iterfind(".//*[@name='Subject']"):
             for value in subject.findall('value'):
@@ -94,7 +94,7 @@ for asset in input_root.iterfind("asset"):
         ET.SubElement(obraElement, 'frbrer:isRealizedThrough', {'rdf:resource': base_uri + 'recurso/expresion/'+ID})
             
         #crear instancia de expresion
-        expresionElement = ET.SubElement(output_expresion_root, 'rdf:Description', {'rdf:about': base_uri + 'recurso/expresion/'+ID})
+        expresionElement = ET.SubElement(output_expresion_root, 'owl:NamedIndividual', {'rdf:about': base_uri + 'recurso/expresion/'+ID})
         #agregar type
         ET.SubElement(expresionElement, 'rdf:type', {'rdf:resource': 'frbrer:C1002'})
         language = asset.find(".//*[@name='Language']")
@@ -109,7 +109,7 @@ for asset in input_root.iterfind("asset"):
         ET.SubElement(expresionElement,'frbrer:isEmbodiedln',{'rdf:resource': base_uri + 'recurso/manifestacion/'+ID})
         
         #crear instancia de Manifestacion
-        manifestacionElement = ET.SubElement(output_manifestacion_root, 'rdf:Description', {'rdf:about': base_uri + 'recurso/manifestacion/'+ID})
+        manifestacionElement = ET.SubElement(output_manifestacion_root, 'owl:NamedIndividual', {'rdf:about': base_uri + 'recurso/manifestacion/'+ID})
         #agregar derechos
         right = asset.find(".//*[@name='Rights']")
         rightElement = ET.SubElement(manifestacionElement, 'dc:rights')
